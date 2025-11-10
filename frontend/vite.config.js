88308443dd6251ba8fc8f124c1083f51c562aa53
Saurabhsinghsawariya <97609934+Saurabhsinghsawariya@users.noreply.github.com>
2025-11-10 15:01:49 +0530

    correct backend link  npm audit fix --force Run `npm audit` for details. ==> Running build command 'npm install; npm run build'... up to date, audited 177 packages in 1s 36 packages are looking for funding   run `npm fund` for details 2 moderate severity vulnerabilities To address all issues (including breaking changes), run:   npm audit fix --force Run `npm audit` for details. > frontend@0.0.0 build > vite build vite v5.4.21 building for production... transforming... ✓ 96 modules transformed. rendering chunks... computing gzip size... dist/index.html                   0.72 kB │ gzip:  0.40 kB dist/assets/index-BcvfHdqx.css    6.21 kB │ gzip:  1.77 kB dist/assets/index-OTjOQEDM.js   256.32 kB │ gzip: 83.87 kB ✓ built in 2.46s ==> Publish directory .dist does not exist! ==> Build failed 😞 ==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Your backend server
+        target: 'https://vibe-eccommerce-backend.onrender.com', // Your backend server
         changeOrigin: true,
         secure: false,
       }
